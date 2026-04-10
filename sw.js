670e25771736d79c8ddc378518277ded223401da
cheesecheesecheese95 <cheese@vntgcorp.com>
2026-04-10 17:53:12 +0900

    fix: bump SW cache to v2 to force client update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pvx-on-v1';
+const CACHE = 'pvx-on-v2';
 self.addEventListener('install', e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/','index.html'])).catch(()=>{}));
